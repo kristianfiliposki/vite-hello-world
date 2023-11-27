469ea5ef6e670c620ed4fc0fc1546a7f748ef862
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-11-27 18:31:31 +0100

    first commit

---
+++ vite-project/src/App.vue
@@ -0,0 +1,21 @@
+<script >
+import prova from './components/prova.vue'
+import saluto from './components/saluto.vue'
+
+export default {
+  components: {
+    saluto,
+    prova
+  }
+}
+</script>
+
+
+
+<template>
+  <h1>CIAO VUE.JS</h1>
+  <saluto />
+  <prova />
+</template>
+
+<style scoped></style>
---
+++ vite-project/src/components/prova.vue
@@ -0,0 +1,5 @@
+<script></script>
+<template>
+    <h2>proviamo a fare i pro</h2>
+</template>
+<style></style>
